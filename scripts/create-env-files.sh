a6d108c51b46c0a7f3063242358ba98a361b8c0a
Enrico Monte <38566410+enrimon15@users.noreply.github.com>
2024-11-22 15:48:41 +0100

    chore(CACI-173): gitignore and root path in create-env-files.sh script

--- scripts/create-env-files.sh
+++ scripts/create-env-files.sh
@@ -4,14 +4,17 @@
 RED='\033[0;31m'
 NOCOLOR='\033[0m'
 
+# Get root directory where there are brand folders (prev path from the script location)
+root_dir="$(cd "$(dirname "$0")/.." && pwd)"
+
 # Find all brand directories (that contain a manifest.json file)
 brand_folders=$(bash get-brand-folders.sh)
 
 # Create .env based on .env.example to each folder (if not exists yet)
 for folder in $brand_folders; do
-  if [ ! -f "$folder/.env" ]; then
-    if [ -f "$folder/.env.example" ]; then
-      cp "$folder/.env.example" "$folder/.env"
+  if [ ! -f "$root_dir/$folder/.env" ]; then
+    if [ -f "$root_dir/$folder/.env.example" ]; then
+      cp "$root_dir/$folder/.env.example" "$root_dir/$folder/.env"
     else
       echo -e "${RED}.env.example not found!${NOCOLOR}"
     fi
