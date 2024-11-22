c9e6f0f6261f84a4973b016311dbd1b46c234275
Enrico Monte <38566410+enrimon15@users.noreply.github.com>
2024-11-22 16:08:00 +0100

    feat(CACI-174): ci/cd check action with common reusable component
    it checks pr title, lint, and if brand manifest version is updated (if brand folder is modified)

---
+++ scripts/validate-manifests-version.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# set echo color
+RED='\033[0;31m'
+NOCOLOR='\033[0m'
+
+# Check all required parameters
+if [ "$#" -lt 2 ]; then
+  echo -e "${RED}Usage: $0 <BASE_BRANCH> <CHANGED_FOLDERS>${NOCOLOR}"
+  exit 1
+fi
+
+BASE_BRANCH=$1
+CHANGED_FOLDERS=$2
+
+echo "Base Branch: $BASE_BRANCH"
+echo -e "Changed Folders: $CHANGED_FOLDERS \n"
+
+for FOLDER in $CHANGED_FOLDERS; do
+    MANIFEST="$FOLDER/manifest.json"
+
+    if [[ -f "$MANIFEST" ]]; then
+        echo "Validating $MANIFEST"
+
+        CURRENT_VERSION=$(jq -r '.version' "$MANIFEST")
+        BASE_VERSION=$(git show "$BASE_BRANCH:$MANIFEST" 2>/dev/null | jq -r '.version' || echo "null")
+
+        if [ "$CURRENT_VERSION" == "$BASE_VERSION" ]; then
+            echo -e "${RED}Error: Version in $MANIFEST is not updated ($CURRENT_VERSION).${NOCOLOR}"
+            exit 1
+        else
+            echo -e "Version updated in $MANIFEST: $BASE_VERSION -> $CURRENT_VERSION \n"
+        fi
+    else
+        echo -e "No manifest.json found in $FOLDER. Skipping.."
+    fi
+done
