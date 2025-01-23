3fb81c2ae8355f3f3d652b7d67e4c79cbf4aa4eb
Enrico Monte <38566410+enrimon15@users.noreply.github.com>
2025-01-23 12:57:05 +0100

    feat(CACI-262 HC-IO): refactor beadcrumb replaced with back link (all content is aligned to sx); search result replaced with category link

--- hc_io/assets/footer-column-links.js
+++ hc_io/assets/footer-column-links.js
@@ -3,18 +3,15 @@
 
   function getLinks(columnNumber) {
     const links = document.querySelector(`[data-footer-${columnNumber}-column-links]`);
-    console.log(links);
     if (!links) {
       return true;
     }
     const trimLinks = links.innerText.trim();
-    console.log(trimLinks);
 
     return trimLinks.split(';').map((link) => {
       const linkParam = link.split('|');
       const linkName = linkParam[0].trim();
       const linkURL = linkParam[1].trim();
-      console.log(linkName, linkURL);
 
       return {
         linkName,
