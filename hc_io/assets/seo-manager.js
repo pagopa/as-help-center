de19ffaeae44cca40d534995d8f43739047b064a
Enrico Monte <38566410+enrimon15@users.noreply.github.com>
2025-02-14 14:08:56 +0100

    feat(CACI-294 CACI-295): open graph tags outside of seo-manager script it's not possible to manage open graph tags there because they need to be in source code before js injection

--- hc_io/assets/seo-manager.js
+++ hc_io/assets/seo-manager.js
@@ -1,69 +1,28 @@
-const ogProps = {
-  type: 'og:type',
-  title: 'og:title',
-  description: 'og:description',
-  url: 'og:url',
-  siteName: 'og:site_name',
-  image: 'og:image'
+// script for adding a meta description
+// it's not possible to manage open graph there because they need to be in source code before js injection
+
+// function for creating meta tag
+let setMeta = (attr, attrValue, content) => {
+  let head = document.getElementsByTagName('head')[0];
+  let existingMeta = document.querySelector(`meta[${attr}="${attrValue}"]`);
+
+  if (existingMeta) {
+    existingMeta.setAttribute('content', content);
+  } else {
+    let metaTag = document.createElement('meta');
+    metaTag.setAttribute(attr, attrValue);
+    metaTag.setAttribute('content', content);
+    head.prepend(metaTag);
+  }
 };
 
-// script for adding a meta description
-document.addEventListener('DOMContentLoaded', function () {
-  // function for creating meta tag
-  let setMeta = (attr, attrValue, content) => {
-    let head = document.getElementsByTagName('head')[0];
-    let existingMeta = document.querySelector(`meta[${attr}="${attrValue}"]`);
-
-    if (existingMeta) {
-      existingMeta.setAttribute('content', content);
-    } else {
-      let metaTag = document.createElement('meta');
-      metaTag.setAttribute(attr, attrValue);
-      metaTag.setAttribute('content', content);
-      head.prepend(metaTag);
-    }
-  };
-
-  const siteTitle = document.querySelector('title').innerText;
-  // eslint-disable-next-line no-undef
-  const ogImg = ogImageUrl;
-
-  // for the homepage
-  if (LotusUtils.isHomePage()) {
-    const description =
-      "Scopri le guide pratiche e il supporto ufficiale per usare l'app e risolvere problemi nel Centro assistenza di IO.";
-    const ogDescription = 'Supporto e guide ufficiali per usare l’app IO e risolvere problemi.';
-    // description
-    setMeta('name', 'description', description);
-    // open graph
-    setMeta('property', ogProps.type, 'website');
-    setMeta('property', ogProps.title, siteTitle);
-    setMeta('property', ogProps.description, ogDescription);
-    setMeta('property', ogProps.url, window.location.origin);
-    setMeta('property', ogProps.image, ogImg);
-  }
-  // section page
-  else if (LotusUtils.isSectionPage()) {
-    // open graph
-    const siteName = siteTitle.split(' - ')[1];
-    const title = siteTitle.split(' - ')[0];
-    setMeta('property', ogImg.type, 'website');
-    setMeta('property', ogImg.title, title);
-    setMeta('property', ogImg.siteName, siteName);
-    setMeta(
-      'property',
-      ogImg.description,
-      document.querySelector('meta[name="description"]').innerText
-    );
-    setMeta('property', ogProps.url, window.location.origin);
-    setMeta('property', ogProps.image, ogImg);
-  }
-  // articles page
-  else if (LotusUtils.isArticlePage()) {
-    setMeta('property', ogProps.image, ogImg);
-  }
-  // info privacy page
-  else if (LotusUtils.isCustomPage('info_privacy')) {
-    setMeta('name', 'robots', 'noindex, follow');
-  }
-});
+// for the homepage
+if (LotusUtils.isHomePage()) {
+  const description =
+    "Scopri le guide pratiche e il supporto ufficiale per usare l'app e risolvere problemi nel Centro assistenza di IO.";
+  setMeta('name', 'description', description);
+}
+// info privacy page
+else if (LotusUtils.isCustomPage('info_privacy')) {
+  setMeta('name', 'robots', 'noindex, follow');
+}
