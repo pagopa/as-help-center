2b31f238ea4b64ed8aeacb913bfc4bf546fc2290
Enrico Monte <38566410+enrimon15@users.noreply.github.com>
2025-03-03 12:48:26 +0100

    feat(HC-SEND): setup help center copy theme from hc_io, footer medium link, seo and og, sections columns and icons, color system, hero, favicon, header

---
+++ hc_send/assets/seo-manager.js
@@ -0,0 +1,28 @@
+// script for adding a meta description
+// it's not possible to manage open graph there because they need to be in source code before js injection
+
+// function for creating meta tag
+let setMeta = (attr, attrValue, content) => {
+  let head = document.getElementsByTagName('head')[0];
+  let existingMeta = document.querySelector(`meta[${attr}="${attrValue}"]`);
+
+  if (existingMeta) {
+    existingMeta.setAttribute('content', content);
+  } else {
+    let metaTag = document.createElement('meta');
+    metaTag.setAttribute(attr, attrValue);
+    metaTag.setAttribute('content', content);
+    head.prepend(metaTag);
+  }
+};
+
+// for the homepage
+if (LotusUtils.isHomePage()) {
+  const description =
+    'Scopri le guide pratiche e il supporto ufficiale per usare la piattaforma e risolvere problemi nel Centro assistenza di SEND';
+  setMeta('name', 'description', description);
+}
+// info privacy page
+else if (LotusUtils.isCustomPage('info_privacy')) {
+  setMeta('name', 'robots', 'noindex, follow');
+}
