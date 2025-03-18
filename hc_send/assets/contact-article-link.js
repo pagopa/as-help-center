d1d402c0e636f6fdd4187652c35b06f369bc5bcd
Enrico Monte <38566410+enrimon15@users.noreply.github.com>
2025-03-18 12:09:31 +0100

    feat: back button to previous article on contact article

--- hc_send/assets/contact-article-link.js
+++ hc_send/assets/contact-article-link.js
@@ -4,8 +4,27 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const contactArticleId = 33395906409745;
 
+  // set contact link to link above each article
   let link = document.querySelector('#cr-article-subtitle a');
   if (link) {
-    link.href = `${baseUrlArticle}/${contactArticleId}`;
+    const articleId = document.querySelector('[data-article-id]').innerHTML.trim();
+    const articleTitle = document.querySelector('[data-article-title]').innerHTML.trim();
+    link.href = `${baseUrlArticle}/${contactArticleId}?prevTitle=${articleTitle}&prevId=${articleId}`;
+  }
+
+  // if we are into contact article set back link as prevoius article
+  if (window.location.href.includes(`${baseUrlArticle}/${contactArticleId}`)) {
+    let backLink = document.querySelector('#article-back-link');
+
+    let params = new URLSearchParams(window.location.search);
+    let prevArticleId = params.get('prevId');
+    let prevArticleTitle = params.get('prevTitle');
+
+    let backLinkLabel = document.querySelector('#article-back-link #section-title');
+
+    if (backLink && prevArticleId && prevArticleTitle && backLinkLabel) {
+      backLink.href = `${baseUrlArticle}/${prevArticleId}`;
+      backLinkLabel.innerText = prevArticleTitle;
+    }
   }
 });
