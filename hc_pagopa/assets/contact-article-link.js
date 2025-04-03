0456a9a7d732bfe9fede06cab523bc14ccf9009f
Enrico Monte <38566410+enrimon15@users.noreply.github.com>
2025-04-03 12:52:48 +0200

    feat: add contact articles ids and script refactor

--- hc_pagopa/assets/contact-article-link.js
+++ hc_pagopa/assets/contact-article-link.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
+  const currentPathname = window.location.pathname;
   const locale = LotusUtils.getLocale();
   const baseUrlArticle = `/hc/${locale}/articles`;
 
-  const contactArticleIds = [31958790454417, 31958822849169];
+  const supportArticle = 34047454046097; // contattare l'assistenza
+  const paymentChannelSupportArticle = 34047636446737; // ottenere supporto per canali di pagamento abilitati
+  const contactArticleIds = [supportArticle, paymentChannelSupportArticle];
 
-  // set contact link to link above each article
+  // set contact link to link above each article page
   let links = document.querySelectorAll('#cr-article-subtitle a');
   if (links) {
     links.forEach((link, index) => {
@@ -14,23 +17,46 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 
-  // if we are into contact article set back link as previous article
-  if (contactArticleIds.some((id) => window.location.href.includes(`${baseUrlArticle}/${id}`))) {
+  // if we are into contact article set back link as previous article (if we come from an article) or as previous page (if we come from home or section page)
+  if (contactArticleIds.some((id) => currentPathname.includes(`${baseUrlArticle}/${id}`))) {
     let backLink = document.querySelector('#article-back-link');
 
     let params = new URLSearchParams(window.location.search);
-    let prevArticleId = params.get('prevId');
-    let prevArticleTitle = params.get('prevTitle');
+    let prevArticleId = params.get('prevId'); // if we come from an article
+    let prevPageLink = params.get('prevPage'); // if we come from home or section page
+    let prevPageTitle = params.get('prevTitle');
 
     let backLinkLabel = document.querySelector('#article-back-link #section-title');
 
-    if (backLink && prevArticleId && prevArticleTitle && backLinkLabel) {
+    // if we come from an article
+    if (backLink && prevArticleId && prevPageTitle && backLinkLabel) {
       backLink.href = `${baseUrlArticle}/${prevArticleId}`;
-      backLinkLabel.innerText = prevArticleTitle;
+      backLinkLabel.innerText = prevPageTitle;
+    }
+
+    // if we come from home or section page
+    if (backLink && prevPageLink && prevPageTitle && backLinkLabel) {
+      backLink.href = prevPageLink;
+      backLinkLabel.innerText = prevPageTitle;
     }
 
     // hide article contacts and add a bottom space
     document.querySelector('#article-contact-box').classList.add('hidden');
     document.querySelector('#article-no-contact-box').classList.remove('hidden');
   }
+
+  // article links contact box (home, section)
+  let contactLink = document.querySelector('#support-article-link');
+  let otherContactLink = document.querySelector('#payment-channel-support-article-link');
+  if (contactLink && otherContactLink) {
+    const prevPageTitle = currentPathname.includes('/sections') ? getSectionNameByUrl() : 'Home';
+    contactLink.href = `${baseUrlArticle}/${supportArticle}?prevTitle=${prevPageTitle}&prevPage=${currentPathname}`;
+    otherContactLink.href = `${baseUrlArticle}/${paymentChannelSupportArticle}?prevTitle=${prevPageTitle}&prevPage=${currentPathname}`;
+  }
 });
+
+function getSectionNameByUrl() {
+  const sectionEndpoint = window.location.pathname.split('/sections').filter(Boolean).pop();
+  const sectionName = sectionEndpoint.split('-').slice(1).join(' ');
+  return sectionName;
+}
